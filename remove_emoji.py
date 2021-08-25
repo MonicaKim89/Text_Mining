5d895de54573eab48149b8a459b94edb550a5cc6
MonicaKim89 <monicakim89@naver.com>
2021-08-25 10:55:44 +0900

    korean

--- remove_emoji.py
+++ remove_emoji.py
@@ -8,6 +8,5 @@ def remove_emoji(string):
                            u"\U0001F680-\U0001F6FF"  # transport & map symbols
                            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                            u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
                            "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', string)
