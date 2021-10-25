e3c057297fc363f335e67f036c8f41e346357902
Monicakim89 <monicakim89@naver.com>
2021-10-26 01:46:52 +0900

    Create nlp_processing.py

---
+++ nlp_processing.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.python.client import device_lib
+
+
+import torch
+print(torch.cuda.is_available())
+
+
+import transformers
+
+SEED = 1337
+
+def set_seeds(seed=SEED):
+    os.environ['PYTHONHASHSEED']= str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+def set_global_determinism(seed=SEED):
+    set_seeds(seed=seed)
+    os.environ['TF_DETERMINISTIC_OPS']= '1'
+    os.environ['TF_CUDNN_DETERMINISTIC']= '1'
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
+
+# Call the above function with seed value
+set_global_determinism(seed=SEED)
+
+gpu_check()
+
