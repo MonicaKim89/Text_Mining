d9a300df2cb7dbe9b54bddd937d97938cdb2ac77
Monica Kim <monicakim89@naver.com>
2021-11-28 23:35:17 +0900

    multipleIterator.py

---
+++ text_py/multipleIterator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class MultipleIterator:
+    def __init__(self, stop, multiple):
+        self.stop = stop
+        self.multiple = multiple
+        self.current = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.current += 1
+        if self.current * self.multiple < self.stop:
+            return self.current * self.multiple
+        else:
+            raise StopIteration
+                                                     
+for i in MultipleIterator(30, 5):
+    print(i, end=' ')
+
